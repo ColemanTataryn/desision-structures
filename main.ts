443cab80986d3458a059d57de9aac9fcbfb87699
ColemanTataryn <coleman.tataryn@stu.ocsb.ca>
2025-02-27 11:02:40 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    competitor = receivedNumber
+    basic.showNumber(competitor)
+})
+let competitor = 0
+radio.setGroup(15)
+basic.forever(function () {
+	
+})
